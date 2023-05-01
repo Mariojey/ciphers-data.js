310219de2b41911ede0c4268e5835148406e045f
Mariusz Jacek <marios.yaceg@gmail.com>
2023-05-01 11:45:06 +0200

    Refactorization

--- modules/Caesar.js
+++ modules/Caesar.js
@@ -1,47 +1,35 @@
-class Caesar {
-
-    constructor(arg, key) {
-        this.arg = arg;
-        this.key = key;
-    }
-
-    encode() {
-        const plainText = this.arg;
-        const key = this.key
-        let arrayOfCharCodes = []
-        let codedArray = []
-        for (let i = 0; i < plainText.length; i++) {
-            arrayOfCharCodes.push(plainText.charCodeAt(i))
-            let charAsciiCode = parseInt(arrayOfCharCodes[i]) + parseInt(key);
-            while (charAsciiCode > 126) {
-                charAsciiCode -= 96;
-            }
-            let char = String.fromCharCode(charAsciiCode)
-            codedArray.push(char)
+function encode(plainText, key) {
+    let arrayOfCharCodes = []
+    let codedArray = []
+    for (let i = 0; i < plainText.length; i++) {
+        arrayOfCharCodes.push(plainText.charCodeAt(i))
+        let charAsciiCode = parseInt(arrayOfCharCodes[i]) + parseInt(key);
+        while (charAsciiCode > 126) {
+            charAsciiCode -= 96;
         }
-        let result = codedArray.toString().replace(/[,]/g, '')
-
-        return result
-
+        let char = String.fromCharCode(charAsciiCode)
+        codedArray.push(char)
     }
+    let result = codedArray.toString().replace(/[,]/g, '')
 
-    decode() {
-        const encodedText = this.arg;
-        const key = this.key;
-        let arrayOfCharCodes = []
-        let decodedArray = []
-        for (let i = 0; i < encodedText.length; i++) {
-            arrayOfCharCodes.push(encodedText.charCodeAt(i))
-            let charAsciiCode = parseInt(arrayOfCharCodes[i]) - parseInt(key);
-            while (charAsciiCode < 40) {
-                charAsciiCode += 96
-            }
-            let char = String.fromCharCode(charAsciiCode)
-            decodedArray.push(char)
-        }
-        let result = decodedArray.toString().replace(/[,]/g, '')
+    return result
 
-        return result
-    }
 }
-module.exports = Caesar
+
+function decode(encodedText, key) {
+    let arrayOfCharCodes = []
+    let decodedArray = []
+    for (let i = 0; i < encodedText.length; i++) {
+        arrayOfCharCodes.push(encodedText.charCodeAt(i))
+        let charAsciiCode = parseInt(arrayOfCharCodes[i]) - parseInt(key);
+        while (charAsciiCode < 40) {
+            charAsciiCode += 96
+        }
+        let char = String.fromCharCode(charAsciiCode)
+        decodedArray.push(char)
+    }
+    let result = decodedArray.toString().replace(/[,]/g, '')
+    return result
+}
+
+module.exports = { encode, decode }
